2a0e5f7e127e447ff6b3ea6c6e4326ab172a427d
iosebi2899 <45330280+iosebi2899@users.noreply.github.com>
2021-08-21 19:27:58 +0400

    vaax
    dasfs

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ $('.burger').click(function() {
       var val = $(this).val();
       if(val === "other") {
           $(".other-position").show();
+          $('#other-position').prop("required", true);
       }else{
         $(".other-position").hide();
       }
@@ -18,23 +19,77 @@ $('.burger').click(function() {
   });
   $(function () {
     $("#region").focus(function() {
-        $('.arrow').toggleClass('active-arrow');
+        $('.arrow').addClass('active-arrow');
     });
     $("#region").change(function() {
-        $('.arrow').toggleClass('active-arrow');
+        $('.arrow').removeClass('active-arrow');
+        $(".select").removeClass('active-label');
+        $(".regionform").removeClass('active-select');
+        $("#correct-region").css({"display":"none"});
+    });
+    $("#region").blur(function() {
+        $('.arrow').removeClass('active-arrow');
     });
   });
   $(function () {
     $("#position").focus(function() {
-        $('.arrow2').toggleClass('active-arrow');
+        $('.arrow2').addClass('active-arrow');
     });
     $("#position").change(function() {
-        $('.arrow2').toggleClass('active-arrow');
+        $('.arrow2').removeClass('active-arrow');
+    });
+    $("#position").blur(function() {
+        $('.arrow2').removeClass('active-arrow');
     });
   });
-  $(function () {
-    $("#body").click(function() {
-        $('.arrow2').removeClass('active-arrow');
-        $('.arrow').removeClass('active-arrow');
+  $(function() {
+    $("#submit").click(function() {
+        if($("#chosen").is(":selected")) {
+            $("#correct-region").css({"display":"block"});
+            $(".select").addClass('active-label');
+            $(".regionform").addClass('active-select');
+            $('html, body').animate({
+                scrollTop: $(".select").offset().top
+            }, 500);
+        }
+        else {
+            $("#correct-region").css({"display":"none"});
+        }
+    });
+});
+  function verifyPassword() {  
+    var pw = document.getElementById("pswd").value;  
+    //check empty password field  
+    if(pw == "") {  
+       document.getElementById("message").innerHTML = "გთხოვთ ჩაწეროთ პაროლი";  
+       return false;  
+    }  
+     
+   //minimum password length validation  
+    if(pw.length < 8) {  
+       document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";  
+       return false;  
+    }  
+    
+  //maximum length of password validation  
+    if(pw.length > 15) {  
+       document.getElementById("message").innerHTML = "**Password length must not exceed 15 characters";  
+       return false;  
+    } else {  
+       alert("Password is correct");  
+    }  
+  } 
+  $("#submit").submit(function(e) {
+});
+  $(function() {
+    $('#submit').on("submit", function() {
+      if (verifyPassword() == false) {
+        verifyPassword();
+        return false;
+      } else {
+        alert("Your code is saved");
+        return true;
+      }
+      preventDefault();
     });
   });
